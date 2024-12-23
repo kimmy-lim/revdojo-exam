77bfb1a6fcf079657c5d9ae311964eaf42b59429
kimmy-lim <kdlim948@gmail.com>
2024-12-24 00:35:17 +0800

    #3 - get vehicle details and create statistics api

--- api/tests.py
+++ api/tests.py
@@ -2,8 +2,7 @@ from django.test import TestCase
 
 # Create your tests here.
 from django.urls import reverse
-from django.utils import timezone
-from .models import Vehicle
+from .models import Vehicle, Statistics
 import json
 
 class VehicleAPITestCase(TestCase):
@@ -20,10 +19,36 @@ class VehicleAPITestCase(TestCase):
         }
         self.vehicle = Vehicle.objects.create(**self.vehicle_data)
 
+        self.vehicle_stats_data = {
+            'vin': self.vehicle,
+            'source': 'ABC',
+            'vdp': 10,
+            'srp': 20,
+        }
+        self.statistics = Statistics.objects.create(**self.vehicle_stats_data)
+
+        self.vehicle_stats_data = {
+            'vin': self.vehicle,
+            'source': 'ABC',
+            'vdp': 110,
+            'srp': 220,
+        }
+        Statistics.objects.create(**self.vehicle_stats_data)
+
+        self.vehicle_stats_data = {
+            'vin': self.vehicle,
+            'source': 'XYZ',
+            'vdp': 300,
+            'srp': 150,
+        }
+        Statistics.objects.create(**self.vehicle_stats_data)
+        
         # Define URL endpoints
         self.create_vehicle = reverse('create-vehicle')
         self.update_vehicle_details = reverse('update-vehicle', kwargs={'vin': self.vehicle.vin})
         self.update_vehicle_status = reverse('sold-vehicle', kwargs={'vin': self.vehicle.vin})
+        self.create_vehicle_statistics = reverse('vehicle-statistics')
+        self.vehicle_details = reverse('vehicle-statistics')
 
     def test_create_Vehicle(self):
         # Test POST method to create a new Vehicle
@@ -37,12 +62,13 @@ class VehicleAPITestCase(TestCase):
             'photos_count': 8,
         }
         response = self.client.post(self.create_vehicle, new_vehicle_data, content_type='application/json')
+        print(response.json())
         self.assertEqual(response.status_code, 200)
 
-    def test_update_Vehicle(self):
+    def test_update_vehicle(self):
         # Test PUT method to update an existing Vehicle
         updated_data = {
-            'vin': 10000000000000004,
+            'vin': 10000000000000001,
             'stock_number': 1000000004,
             'vehicle_type': 'USED',
             'description': 'Used Toyota Vios',
@@ -51,12 +77,13 @@ class VehicleAPITestCase(TestCase):
             'photos_count': 9,
         }
         response = self.client.put(self.update_vehicle_details, updated_data, content_type='application/json')
+        print(response.json())
         self.assertEqual(response.status_code, 200)
     
     def test_update_vehicle_status(self):
         # Test PATCH method to update an existing Vehicle
         updated_data = {
-            'vin': 10000000000000004,
+            'vin': 10000000000000001,
             'stock_number': 1000000004,
             'vehicle_type': 'USED',
             'description': 'Used Toyota Vios',
@@ -65,4 +92,63 @@ class VehicleAPITestCase(TestCase):
             'photos_count': 9,
         }
         response = self.client.patch(self.update_vehicle_status, updated_data, content_type='application/json')
+        print(response.json())
         self.assertEqual(response.status_code, 200)
+
+    def test_create_vehicle_statistic(self):
+        new_stats_data = {
+            'vin': 10000000000000001,
+            'source': 'XYZ',
+            'vdp': 30,
+            'srp': 205,
+        }
+        response = self.client.post(self.create_vehicle_statistics, new_stats_data, content_type='application/json')
+        print(response.json())
+        self.assertEqual(response.status_code, 200)
+
+    def test_filter_by_vehicle_vin(self):
+        response = self.client.get(self.vehicle_details, {'vin': 10000000000000001})
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(data['data'][0]['VIN'], '10000000000000001')
+    
+    def test_filter_by_stock_number(self):
+        response = self.client.get(self.vehicle_details, {'stock_number': 1000000001})
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(data['data'][0]['Stock Number'], '1000000001')
+    
+    def test_filter_by_source(self):
+        response = self.client.get(self.vehicle_details, {'source': 'ABC'})
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(data['data'][0]['Source'], 'ABC')
+    
+    def test_filter_by_vehicle_type(self):
+        response = self.client.get(self.vehicle_details, {'vehicle_type': 'NEW'})
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(data['data'][0]['Vehicle Type'], 'NEW')
+    
+    def test_filter_with_start_date(self):
+        response = self.client.get(self.vehicle_details, {'start_date': '2024-12-24'})
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+    
+    def test_filter_with_end_date(self):
+        response = self.client.get(self.vehicle_details, {'end_date': '2024-12-24'})
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+    
+    def test_filter_by_date_range(self):
+        response = self.client.get(self.vehicle_details, {'start_date': '2024-12-24', 'end_date': '2024-12-24'})
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+    
+    def test_combined_filter(self):
+        filters = {'vin': 10000000000000001, 'source': 'ABC', 'vehicle_type': 'NEW', 
+                   'start_date': '2024-12-24', 'end_date': '2024-12-24'}
+        response = self.client.get(self.vehicle_details, filters)
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+    
--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404
 from django.core.exceptions import ValidationError
 from django.http import JsonResponse
 from django.views import View
+from django.db.models import Sum
 
 from .models import Vehicle, Statistics
 
@@ -62,6 +63,81 @@ class VehicleView(View):
 
         vehicle.save()
         return JsonResponse({'message': 'Vehicle status updated'}, status=200)
+    
+    def http_method_not_allowed(self, request, *args, **kwargs):
+        return JsonResponse({"error": "Method not allowed. Please use POST."}, status=405)
+
+class VehicleStatisticsView(View):
+    def post(self, request, *args, **kwargs):
+        if request.content_type == 'application/json':
+            try:
+                data = json.loads(request.body) 
+            except json.JSONDecodeError: 
+                return handle_invalid_json()
+            
+            data = {k.lower(): v for k, v in data.items()}
+            if 'vin' not in data:
+                return JsonResponse({'error': f'Invalid JSON format. VIN field is required.'}, status=400)
+            
+            vehicle = get_object_or_404(Vehicle, vin=data['vin'])
+            data['vin'] = vehicle
+            new_statistic = Statistics.objects.create(**data)
+
+            try:
+                new_statistic.full_clean()
+            except ValidationError as e:
+                return JsonResponse({'error': f'Invalid JSON format. {e}'}, status=400)
+
+            new_statistic.save()
+            return JsonResponse({'message': 'Vehicle statistics created'}, status=200)
+        else:
+            return handle_invalid_json()
+    
+    def get(self, request):
+        vin = request.GET.get('vin', None)
+        stock_number = request.GET.get('stock_number', None)
+        vehicle_type = request.GET.get('vehicle_type', None)
+        start_date = request.GET.get('start_date', None)
+        end_date = request.GET.get('end_date', None)
+        source = request.GET.get('source', None)
+
+        vehicles = Vehicle.objects.filter(status='ONS')
+        if vin:
+            vehicles = vehicles.filter(vin=vin)
+        if stock_number:
+            vehicles = vehicles.filter(stock_number=stock_number)
+        if vehicle_type:
+            vehicles = vehicles.filter(vehicle_type=vehicle_type)
+        
+        data = []
+        for vehicle in vehicles:
+            statistics = vehicle.stats.all()
+            if source:
+                statistics = statistics.filter(source=source)
+            if start_date:
+                try:
+                    datetime.strptime(start_date, '%Y-%m-%d')
+                    statistics = statistics.filter(date__gte=start_date)
+                except ValueError as e:
+                    return JsonResponse({'error': 'Invalid date format. Use YYYY-MM-DD'}, status=200)
+            if end_date:
+                try:
+                    datetime.strptime(end_date, '%Y-%m-%d')
+                    statistics = statistics.filter(date__lte=end_date)
+                except ValueError as e:
+                    return JsonResponse({'error': 'Invalid date format. Use YYYY-MM-DD'}, status=200)
+            
+            sources = statistics.values_list('source', flat=True).distinct()
+            for s in sources:
+                subset_stat = statistics.filter(source=s)
+                total_vdp = subset_stat.aggregate(total_vdp=Sum('vdp'))['total_vdp'] or 0
+                total_srp = subset_stat.aggregate(total_srp=Sum('srp'))['total_srp'] or 0
+                data.append({'VIN': vehicle.vin, 'Stock Number': vehicle.stock_number, 'Start Date': vehicle.start_date, \
+                          'Vehicle Type': vehicle.vehicle_type, 'Description': vehicle.description, \
+                           'Price': vehicle.price, 'Photos Count': vehicle.photos_count,\
+                            'Total VDP Count': total_vdp, 'Total SRP Count': total_srp, 'Source':s})
+        response = {'data': data}
+        return JsonResponse(response, status=200)
     
     def http_method_not_allowed(self, request, *args, **kwargs):
         return JsonResponse({"error": "Method not allowed. Please use POST."}, status=405)
