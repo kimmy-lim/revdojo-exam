39b9908b61c9e33a1771d631314d38662ccbc86a
kimmy-lim <kdlim948@gmail.com>
2024-12-23 21:48:36 +0800

    #2 - create first API

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views  # Import views from your app
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('vehicle', views.VehicleView.as_view(), name='create-vehicle'),
+    path('vehicle/<int:vin>', views.VehicleView.as_view(), name='update-vehicle'),
+    path('vehicle/<int:vin>/sold', views.VehicleView.as_view(), name='sold-vehicle'),
+
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+from django.core.exceptions import ValidationError
 from django.http import JsonResponse
+from django.views import View
 
-def index(request):
-    data = {'message': 'Welcome', 'status': 'success'}
-    return JsonResponse(data, status=200)
+from .models import Vehicle, Statistics
+
+from datetime import datetime
+import json
+
+def handle_invalid_json():
+    return JsonResponse({'error': 'Invalid JSON format'}, status=400)
+
+class VehicleView(View):
+    def post(self, request, *args, **kwargs):
+        if request.content_type == 'application/json':
+            try:
+                data = json.loads(request.body) 
+            except json.JSONDecodeError: 
+                return handle_invalid_json()
+            vehicle_data = {k.lower(): v for k, v in data.items()}
+            new_vehicle = Vehicle.objects.create(**vehicle_data)
+
+            try:
+                new_vehicle.full_clean()
+            except ValidationError as e:
+                return JsonResponse({'error': f'Invalid JSON format. {e}'}, status=400)
+
+            new_vehicle.save()
+            return JsonResponse({'message': 'Vehicle created'}, status=200)
+        else:
+            return handle_invalid_json()
+
+    def put(self, request, vin):
+        if request.content_type == 'application/json':
+            try:
+                data = json.loads(request.body) 
+            except json.JSONDecodeError: 
+                return handle_invalid_json()
+            vehicle = get_object_or_404(Vehicle, vin=vin)
+            for field, val in data.items():
+                if val is not None and field != 'status':
+                    setattr(vehicle, field, val)
+
+            try:
+                vehicle.full_clean()
+            except ValidationError as e:
+                return JsonResponse({'error': f'Invalid JSON format. {e}'}, status=400)
+            
+            vehicle.save()
+            return JsonResponse({'message': 'Vehicle updated'}, status=200)
+        else:
+            return handle_invalid_json()
+    
+    def patch(self, request, vin):
+        vehicle = get_object_or_404(Vehicle, vin=vin)
+        setattr(vehicle, 'status', 'SLD')
+        setattr(vehicle, 'end_date', datetime.now().date())
+
+        vehicle.save()
+        return JsonResponse({'message': 'Vehicle status updated'}, status=200)
+    
+    def http_method_not_allowed(self, request, *args, **kwargs):
+        return JsonResponse({"error": "Method not allowed. Please use POST."}, status=405)
